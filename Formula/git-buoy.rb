a8f19864bd0b68aed3bd6be8d51e8e0c8a9321c5
Mark George <markgeorge0512@gmail.com>
2026-07-13 23:31:29 -0700

    Pin the formula version to 0.1.0

--- Formula/git-buoy.rb
+++ Formula/git-buoy.rb
@@ -2,6 +2,7 @@ class GitBuoy < Formula
   desc "Living terminal harbor for understanding parallel software work"
   homepage "https://github.com/markg-05/git-buoy"
   url "https://github.com/markg-05/git-buoy/releases/download/v0.1.0/git-buoy-v0.1.0-macos-aarch64.tar.gz"
+  version "0.1.0"
   sha256 "dafb53fb838e8c5f4a555847ead9505dd51d38a49edff51f7151e2eedcdb54e0"
   license "MIT"
 
