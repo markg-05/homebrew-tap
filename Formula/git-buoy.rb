a06e4fd4d38735b232aa45e34633fb9c6e9009d3
Mark George <markgeorge0512@gmail.com>
2026-07-13 23:29:58 -0700

    Add Git Buoy 0.1.0 formula

---
+++ Formula/git-buoy.rb
@@ -0,0 +1,18 @@
+class GitBuoy < Formula
+  desc "Living terminal harbor for understanding parallel software work"
+  homepage "https://github.com/markg-05/git-buoy"
+  url "https://github.com/markg-05/git-buoy/releases/download/v0.1.0/git-buoy-v0.1.0-macos-aarch64.tar.gz"
+  sha256 "dafb53fb838e8c5f4a555847ead9505dd51d38a49edff51f7151e2eedcdb54e0"
+  license "MIT"
+
+  depends_on arch: :arm64
+  depends_on :macos
+
+  def install
+    bin.install "git-buoy"
+  end
+
+  test do
+    assert_match "git-buoy #{version}", shell_output("#{bin}/git-buoy --version")
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# homebrew-tap
-Homebrew formulae for Git Buoy
+# Git Buoy Homebrew tap
+
+This tap installs the supported Apple Silicon build of
+[Git Buoy](https://github.com/markg-05/git-buoy).
+
+```sh
+brew install markg-05/tap/git-buoy
+```
+
+Then point Git Buoy at a repository:
+
+```sh
+git-buoy path/to/repository
+```
+
+Git and an interactive terminal are required. GitHub observation is optional
+and requires an authenticated `gh` executable.
